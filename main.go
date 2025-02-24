8bc1d5fa6843f932d2d95137614643275270c266
Sidharth <sidharthinfernal@gmail.com>
2025-02-24 15:53:13 +0530

    used chi router and set /hello route

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func main() {
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
