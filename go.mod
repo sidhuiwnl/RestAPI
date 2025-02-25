9c44ad6d606563b3929676beec21c5ff712ae65b
Sidharth <sidharthinfernal@gmail.com>
2025-02-26 00:24:00 +0530

    added data model

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/redis/go-redis/v9 v9.7.1 // indirect
---
+++ model/order.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Order struct {
+	OrderID     uint64     `json:"order_id"`
+	CustomerID  uuid.UUID  `json:"customer_id"`
+	LineItems   []LineItem `json:"line_items"`
+	CreatedAt   *time.Time `json:"created_at"`
+	ShippedAt   *time.Time `json:"shipped_at"`
+	CompletedAt *time.Time `json:"completed_at"`
+}
+
+type LineItem struct {
+	ItemId   uuid.UUID `json:"item_id"`
+	Quantity uint      `json:"quantity"`
+	Price    uint      `json:"price"`
+}
